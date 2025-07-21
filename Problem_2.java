bcb52b457b076d6cbf088f3a1655fca9b630bf55
Zoha243 <mezoha566@gmail.com>
2025-07-21 22:06:23 +0500

    Initial commit - Java practice problems

---
+++ Problem_2.java
@@ -0,0 +1,13 @@
+// Even or Odd check
+class Check{
+
+    public static void main(String[] args){
+	int number =3;
+	if(number%2==0){
+	System.out.println("Number is Even");
+	}
+	else{
+	System.out.println("Number is Odd");
+	}
+}
+}
---
+++ Problem_3.java
@@ -0,0 +1,16 @@
+// Name & Age profile print
+class Profile{
+
+    public static void main(String[] args){
+	String name= "Zoha";
+	String last = "Memon";
+	int age = 23;
+	String degree = "Computer Systems Engineering";
+	String status = "Graduated";
+	System.out.println("Name:"+' '+name);
+	System.out.println("last Name:"+ ' '+last);
+	System.out.println("Age:"+ ' ' +age);
+	System.out.println("Degree:"+' '+degree);
+	System.out.println("Status:"+' '+status);
+}
+}
